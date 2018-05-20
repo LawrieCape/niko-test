877bff85c288ab6a60e01ccc4156570763dc304e
Unknown <leptest@gmail.com>
2018-05-20 08:47:08 +0100

    styles

--- src/components/ProductSections.jsx
+++ src/components/ProductSections.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import Scrollspy from 'react-scrollspy';
 import { StickyContainer, Sticky } from 'react-sticky';
+import ProductPdfs from '../components/product/ProductPdfs';
 import './ProductSections.scss';
 
 
@@ -16,110 +17,104 @@ function slugify(text) {
 }
 
 const ServicesComponent = function ({ sections }) {
-	console.log('sections', sections);
 	let scrollNavList = [];
 
 	if (sections) {
 		scrollNavList = sections.map(section => slugify(section.title));
 	}
-	console.log('scrollNavList: ', scrollNavList);
 
 	return (
 		<div className="product-sections">
-			<StickyContainer>
-				<Sticky>
-					{({ style }) => (
-						<div style={style}>
-							<Scrollspy items={scrollNavList} className="product-page__nav" currentClassName="is-current" componentTag="nav" offset={0}>
-								{sections && sections.map((section, index) => (
-									<div key={index}><a href={`#${slugify(section.title)}`}>{section.title}</a></div>
-								))}
-							</Scrollspy>
-						</div>
-					)}
-
-					{/* <nav className="product-page__nav">
-						{sections && sections.map((section, index) => (
-							<div key={index} className="">
-								<ScrollLink name={slugify(section.title)}>{section.title}</ScrollLink>
+			<StickyContainer className="product-info">
+				<div className="product-info__sidebar">
+					<Sticky>
+						{({ style }) => (
+							<div className="stickydsfdgdfgfd" style={style}>
+								<Scrollspy items={scrollNavList} className="product-page__nav" currentClassName="is-current" componentTag="nav" offset={0}>
+									{sections && sections.map((section, index) => (
+										<div key={index}><a href={`#${slugify(section.title)}`}>{section.title}</a></div>
+									))}
+								</Scrollspy>
 							</div>
-						))}
-					</nav> */}
+						)}
 
-					{/* <nav className="product-page__nav">
-						<ul>
-							<li>
-								<a className="scroll" href="#about">industrial-sliding-doors</a>
-							</li>
-							<li>
-								<a className="scroll" href="#sliding-door-technical-specs">sliding-door-technical-specs</a>
-							</li>
-							<li>
-								<a className="scroll" href="#sliding-door-animations">sliding-door-animations</a>
-							</li>
-							<li>
-								<a className="scroll" href="#sliding-door-fittings">sliding-door-fittings</a>
-							</li>
-						</ul>
-					</nav> */}
-				</Sticky>
-
-				{sections && sections.map((section, index) => (
-					<article key={index} className="product-section" id={slugify(section.title)}>
-						{/* Title */}
-						<h2>{section.title}</h2>
-
-						{/* Intro */}
-						<section className="product-content-group">
-							{/* <h3>Body</h3> */}
-							<ReactMarkdown source={section.body} />
-						</section>
-
-						{/* Images */}
-						<section className="product-content-group">
-							{/* <h3>Images</h3> */}
-							{section.images && section.images.map((image, index2) => (
-								<a key={index2} className="product-image" href={image.imageUrl}>
-									<img key={index2} src={image.imageUrl} alt={image.imageAlt} width="200" height="200" />
-								</a>
-							))}
-						</section>
-
-						{/* PDFs */}
-						<section className="product-content-group">
-							{/* <h3>PDFs</h3> */}
-							{section.pdfs && section.pdfs.map((pdf, index2) => (
-								<a key={index2} className="pdf__link" href={pdf.pdfUrl} target="_blank" rel="noopener">
-									<img className="pdf__image" src={pdf.pdfThumb} alt={pdf.title} width="200" height="200" />
-									<p className="pdf__title">{pdf.title}</p>
-								</a>
-							))}
-						</section>
-
-						{/* Videos */}
-						<section className="product-content-group product-content-group--videos">
-							{/* <h3>Videos</h3> */}
-							{section.videos && section.videos.map((video, index2) => (
-								<div key={index2} className="video__wrapper">
-									<div className="embed-container">
-										<iframe src={`https://www.youtube.com/embed/${video.youTubeId}?showinfo=1&controls=1&rel=0&modestbranding=0&playsinline=1`} title={video.vidDescription} allowFullScreen="allowfullscreen" />
-									</div>
+						{/* <nav className="product-page__nav">
+							{sections && sections.map((section, index) => (
+								<div key={index} className="">
+									<ScrollLink name={slugify(section.title)}>{section.title}</ScrollLink>
 								</div>
 							))}
-						</section>
+						</nav> */}
 
-						{/* Products */}
-						<section className="product-content-group">
-							{/* <h3>Products</h3> */}
-							{section.products && section.products.map((product, index2) => (
-								<a key={index2} className="product" href={product.imageUrl}>
-									<p className="product__title">{product.title}</p>
-									<img className="product__image" src={product.imageUrl} alt={product.title} width="200" height="200" />
-								</a>
-							))}
-						</section>
-					</article>
-				))}
+						{/* <nav className="product-page__nav">
+							<ul>
+								<li>
+									<a className="scroll" href="#about">industrial-sliding-doors</a>
+								</li>
+								<li>
+									<a className="scroll" href="#sliding-door-technical-specs">sliding-door-technical-specs</a>
+								</li>
+								<li>
+									<a className="scroll" href="#sliding-door-animations">sliding-door-animations</a>
+								</li>
+								<li>
+									<a className="scroll" href="#sliding-door-fittings">sliding-door-fittings</a>
+								</li>
+							</ul>
+						</nav> */}
+					</Sticky>
+				</div>
+
+				<div className="product-info__main">
+					{sections && sections.map((section, index) => (
+						<article key={index} className="product-section" id={slugify(section.title)}>
+							{/* Title */}
+							<h2>{section.title}</h2>
+
+							{/* Intro */}
+							<section className="product-content-group">
+								{/* <h3>Body</h3> */}
+								<ReactMarkdown source={section.body} />
+							</section>
+
+							{/* Images */}
+							<section className="product-content-group product-content-group--images">
+								{/* <h3>Images</h3> */}
+								{section.images && section.images.map((image, index2) => (
+									<a key={index2} className="product-image" href={image.imageUrl}>
+										<img key={index2} src={image.imageUrl} alt={image.imageAlt} />
+									</a>
+								))}
+							</section>
+
+							{/* PDFs */}
+							<ProductPdfs pdfs={section.pdfs} />
+
+							{/* Videos */}
+							<section className="product-content-group product-content-group--videos">
+								{/* <h3>Videos</h3> */}
+								{section.videos && section.videos.map((video, index2) => (
+									<div key={index2} className="video__wrapper">
+										<div className="embed-container">
+											<iframe src={`https://www.youtube.com/embed/${video.youTubeId}?showinfo=1&controls=1&rel=0&modestbranding=0&playsinline=1`} title={video.vidDescription} allowFullScreen="allowfullscreen" />
+										</div>
+									</div>
+								))}
+							</section>
+
+							{/* Products */}
+							<section className="product-content-group">
+								{/* <h3>Products</h3> */}
+								{section.products && section.products.map((product, index2) => (
+									<a key={index2} className="product" href={product.imageUrl}>
+										<p className="product__title">{product.title}</p>
+										<img className="product__image" src={product.imageUrl} alt={product.title} width="200" height="200" />
+									</a>
+								))}
+							</section>
+						</article>
+					))}
+				</div>
 
 			</StickyContainer>
 

b72e148939f6bdb821e044abd6e482e9d2a0a1ef
Daniel Chapman <leptest@gmail.com>
2018-06-07 14:44:14 +0100

    netlify form fix

--- src/components/forms/FormNikotrack.jsx
+++ src/components/forms/FormNikotrack.jsx
@@ -5,6 +5,7 @@ const FormNikotrack = function ({ enquiryType, isHidden }) {
 	return (
 		<form className="form enquiry-form" name="contactNikotrack" method="POST" data-enquiry={enquiryType} aria-hidden={isHidden} data-netlify="true">
 			<input type="hidden" name="form-name" value="contactNikotrack" />
+			<label>Don't fill this out: <input name="bot-field" onChange={this.handleChange} /></label>
 			<div data-netlify-recaptcha />
 			<div className="row">
 

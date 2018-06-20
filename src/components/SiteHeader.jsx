81c7a7b046c4cd7e7b509838796a91feb855ea6f
Daniel Chapman <leptest@gmail.com>
2018-06-20 08:57:26 +0100

    site header style updates

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -9,49 +9,9 @@ const SiteHeader = ({ posts }) => {
 	return (
 		<header className="site-header__wrapper">
 
-			<div className="wee-nav__wrapper2">
-				<div className="container-fluid">
-					{/* wee nav */}
-					<div className="wee-nav__wrapper">
-
-						{/* contact us */}
-						<div className="header__contact-us__wrapper">
-							Call Now: <a href="tel:+14016837525">+1 401 683 7525</a>
-						</div>
-
-						<nav className="wee-nav">
-							<Link className="" to="/">
-								Home
-							</Link>
-							<Link className="" to="/latest-news">
-								Latest News
-							</Link>
-							{/* <Link className="" to="/products">
-								Products
-							</Link>
-							<Link className="" to="/services">
-								Services
-							</Link>
-							<Link className="" to="/our-work">
-								Our Work
-							</Link>
-							<Link className="" to="/careers">
-								Careers
-							</Link> */}
-							<Link className="" to="/contact">
-								Contact
-							</Link>
-							<Link className="" to="/contact">
-								Request a Quote
-							</Link>
-						</nav>
-					</div>
-				</div>
-			</div>
-
-
 			<div className="container-fluid">
 				<div className="site-header">
+
 					{/* company logo */}
 					<div className="header-logo">
 						<Link className="header-logo__link" to="/">
@@ -61,6 +21,28 @@ const SiteHeader = ({ posts }) => {
 						</Link>
 					</div>
 
+					<div className="wee-nav__wrapper">
+						{/* wee nav */}
+						<nav className="wee-nav">
+							<Link className="wee-nav__link" to="/">
+								Home
+							</Link>
+							<Link className="wee-nav__link" to="/latest-news">
+								Latest News
+							</Link>
+							<Link className="wee-nav__link" to="/contact">
+								Contact
+							</Link>
+							<Link className="wee-nav__link" to="/contact">
+								Request a Quote
+							</Link>
+						</nav>
+
+						{/* contact us */}
+						<div className="header__contact-us__wrapper">
+							<a href="tel:+14016837525">Call Now: +1 401 683 7525</a>
+						</div>
+					</div>
 
 				</div>
 			</div>
